0e52b60ee3922eadf5d5a4df753b28828ba1e97a
Arunkrishnan <hereisarunkrishnan@gmail.com>
2015-07-22 16:14:45 +0530

    Introduces handlers

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import socket
 from pprint import pprint
 import time 
 import urlparse
+import sys
+from random import randint
+import os
 
 '''
 *****************************************************
@@ -42,6 +45,7 @@ def public(path):
     except IOError:
     	return '',''
 
+
 '''
 ****************************************************
 Server and socket functions
@@ -55,87 +59,128 @@ def start_server(hostname, port):
 	print "server started at port:", port
 	return sock
 
+
 def accept_connection(sock):
+        print "Accept Connection"
 	(client,(ip,port)) = sock.accept()
 	print "connection request from client:", ip
 	data = client.recv(1024)
 	return client, bytes.decode(data)
 
+
+'''
+Parser Functions
+'''
+def head_parser(message):
+    header={}
+    try:
+        for each_line in message.split("\n")[1:]:
+            key, value = each_line.split(":",1)
+            header[key] = value
+    except ValueError:
+        header['content'] = message.rsplit("\n")[-1]
+    return header
+
+
+def request_parser(message):
+    request = {}
+    first = message.split("\n")[0]
+    request["method"]   = first.split()[0]
+    request["path"]     = first.split()[1]
+    request["protocol"] = first.split()[2]
+    request['header'] = head_parser(message.split("\n\n")[0].strip())
+    try:
+        request['body'] = message.rsplit('\n')[-1]
+    except IndexError:
+        print "No Body Part in the request"
+        pass
+    return request
+
+def response_stringify(response):
+    response_string = response['status'] + '\n'
+    keys = [key for key in response if key not in ['status','content']]
+    for key in keys:
+        response_string += key + ':' + response[key] + '\n'
+    response_string += '\n'
+    if 'content' in response:
+        response_string += response['content'] + '\n\n'
+    return response_string
+
 '''
 *******************************************************
 Handler Functions
 '''
-def request_parser(request):
-	parsed_request = {}
-        method = request.split("\n")[0]
-	parsed_request["method"] = method.split()[0]
-        parsed_request["path"] = method.split()[1]
-        parsed_request["protocol"] = method.split()[2]
-        header = request.split("\n\n")[0].strip()
-        try:
-            for each_line in header.split("\n")[1:]:
-                key, value = each_line.split(":",1)
-                parsed_request[key] = value
-        except ValueError:
-            parsed_request['content'] = request.rsplit("\n")[-1]
-	    pass
-	return parsed_request
+def request_handler(message, response):
+    print "Reached Request Handler"
+    request = request_parser(message)
+    print "Got request"
+    print request
+    method_handler(request, response)
 
-def gen_header(response):
-        h = ""
-        if (response == 200):
-                h = "HTTP/1.1 200 OK\n"
-        elif(response == 404):
-                h = "HTTP/1.1 404 Not Found\n"
-        h +="Date: " + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
-        h += "\nConnection: close\n"
-        h += "Server: geekskoolblog_server\n"
-        return h
+def method_handler(request, response):
+    handler = METHOD[request['method']]
+    handler(request, response)
 
-def do_GET(request):
-        content = ""
-	path = request['path']
+
+def get_handler(request, response):
+    content = ""
+    path = request['path']
+    try:
+        content, content_type = Routes[path]()
+    except KeyError:
+        content, content_type = public(path)
+
+    if content:
+        gen_header(200)
+    else:
+        gen_header(404)
+        return 
+    try:
+        response['Content-type'] =  CONTENT_TYPE[content_type]
+        response['content'] = content
+    except KeyError:
+        print "Content Type %r Not defined" %content_type
+
+
+def post_handler(request, response):
+    #length = int(request['Content_Length'])
+    path = request['path']
+    content = urlparse.parse_qs(request['body']) 
+    try:
+        content, content_type = Landing[path](content)
+        if content:
+            gen_header(200)
+        else:
+            gen_header(404)
+            return
         try:
-            content, content_type = Routes[path]()
+            response['Content-type'] = CONTENT_TYPE[content_type]
+            response['content'] = content
         except KeyError:
-            content, content_type = public(path)
- 
-	if content:
-		response = gen_header(200)
-	else:
-		response = gen_header(404)
-		return response
-        try:
-            response += "Content-type: " + Content_Type[content_type.lower()] + "\n\n"
-        except KeyError:
-            print "Content Type %r Not defined" %content_type
+            print "Content Type %r Not defined" %content_type 
+    except:
+        pass
 
-        response += content + "\n\n"
+def head_handler(request, response):
+    pass
 
-        return response
 
-def do_POST(request):
-        length = int(request['Content-Length'])
-	path = request['path']
-        content = urlparse.parse_qs(request['content'])
-        try:
-		content, content_type = Landing[path](content)
-		if content:
-        	        response = gen_header(200)
-	        else:
-               		response = gen_header(404)
-                	return response
-        	try:
-            		response += "Content-type: " + Content_Type[content_type.lower()] + "\n\n"
-        	except KeyError:
-            		print "Content Type %r Not defined" %content_type
+def file_handler(request, response):
+    pass
 
-        	response += content + "\n\n"
-        	return response
 
-	except:
-		pass
-	raise KeyboardInterrupt
+def delete_handler(request, response):
+    pass
+
+
+def gen_header(response_code):
+    if (response_code == 200):
+        response['status']= "HTTP/1.1 200 OK"
+    elif(response_code == 404):
+        response['status'] = "HTTP/1.1 404 Not Found"
+    response['Date'] = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+    response['Connection'] =  'close'
+    response['Server'] = 'geekskoolblog_server'
 
 
 '''
@@ -147,10 +192,14 @@ Routes = {"/": index,
           "/login": login,}
 
 Landing = {"/login_submit" : login_submit,}
-Methods = {'GET': do_GET,
-	   'POST': do_POST,}
 
-Content_Type = {'html': 'text/html',
+METHOD = {'GET'   : get_handler,
+	   'POST'  : post_handler,
+           'DELETE': delete_handler,
+           'HEAD'  : head_handler,
+           'FILE'  : file_handler,}
+
+CONTENT_TYPE = {'html': 'text/html',
 		'css' : 'text/css',
 		'js'  : 'application/javascript',
 		'jpeg': 'image/jpeg',
@@ -166,16 +215,28 @@ main
 
 if __name__ == "__main__":
     try:
-	sock = start_server("127.0.0.1", 8080)
-	sock.listen(2)
-	while True:
-		client, request = accept_connection(sock)
-	   	if request:
-			request = request_parser(request)
-                	response = Methods[request['method']](request)
-                	print "Sending response"
-                	client.send(response)
-                	client.close() 
+        port = 8081
+        os.system('fuser -k ' + str(port) + '/tcp')
+        sock = start_server("127.0.0.1", port)
+        sock.listen(2)
+        while True:
+            print "while True"
+            client_socket, message = accept_connection(sock)
+            print "In main"
+            print "Message:"
+            print message
+            response = {}
+	    if message:
+	        request_handler(message, response)
+                #print "Response:"
+                #print response
+                print "Sending response"
+                response_string = response_stringify(response)
+                #print "Response String:"
+                #print response_string
+                client_socket.send(response_string)
+            client_socket.close() 
     except KeyboardInterrupt:
-	pass
+        print "Bye"
+	sys.exit(0)
             
